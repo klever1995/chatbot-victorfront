135a2ed597b913ff7345bf45f38ade35daaa7833
klever1995 <klever_mix@hotmail.com>
2026-06-23 18:21:53 -0500

    fix entry point

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { AuthProvider } from '../hooks/useAuth';
 
-export const unstable_settings = { anchor: '(tabs)' };
+export const unstable_settings = { initialRouteName: 'index' };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -15,6 +15,7 @@ export default function RootLayout() {
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <Stack>
           {/* Pantallas de autenticación */}
+          <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="pages/login" options={{ headerShown: false }} />
           <Stack.Screen name="pages/registro" options={{ headerShown: false }} />
           
@@ -22,8 +23,6 @@ export default function RootLayout() {
           <Stack.Screen name="pages/admin" options={{ headerShown: false }} />
           <Stack.Screen name="pages/chat" options={{ headerShown: false }} />
           
-          {/* Grupo de tabs (protegido) */}
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
         </Stack>
         <StatusBar style="auto" />
